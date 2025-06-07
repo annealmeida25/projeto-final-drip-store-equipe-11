e3f236841a9078f556540eb1e78449394043d63e
Angelo QS <149454400+angeloqs@users.noreply.github.com>
2025-06-07 15:31:12 -0300

    Criada a primeira estrutura do projeto (#1)
    * Criada a primeira estrutura do projeto
    
    * Criada as components e pages
    
    * Inserido conteúdo para Layout.jsx

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from '../components/Header.jsx'
+import Footer from '../components/Footer.jsx'
+
+// Definindo o componente de layout
+const Layout = ({ children }) => {
+    return (
+    <>
+      {/* Componente do cabeçalho */}
+      <Header />
+
+      {/* Conteúdo dinâmico das páginas */}
+      {children}
+
+      {/* Componente do rodapé */}
+      <Footer />
+    </>
+    );
+};
+
+export default Layout;
