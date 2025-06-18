0a9feb0c406506556c2af4de832dcc438f900647
Angelo QS <149454400+angeloqs@users.noreply.github.com>
2025-06-18 04:50:08 -0300

    Criada array de objeto com a lista de produtos em json (#4)
    * Criada a primeira estrutura do projeto
    
    * Criada as components e pages
    
    * Inserido conteúdo para Layout.jsx
    
    * Criado estrutura para ProductCard/ProductListing
    
    * Criar products.json com dados

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -2,47 +2,23 @@ import 'primeflex/primeflex.css';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/soho-light/theme.css';
 import ProductCard from './ProductCard';
-
+import products from '../data/products.json';
 
 const ProductListing = () => {
   return (
     <>
-      <div className="flex flex-row flex-wrap">
-      <ProductCard
-        image="https://i.zst.com.br/thumbs/12/3d/17/1273029423.jpg"
-        category={"Tênis"}
-        name="Nomad XYZ"
-        price={100}
-        priceDiscount={50}
-      />
-       <ProductCard
-        image="https://i.zst.com.br/thumbs/12/3d/17/1273029423.jpg"
-        category={"Tênis"}
-        name="Nomad XYZ"
-        price={100}
-        priceDiscount={50}
-      />
-       <ProductCard
-        image="https://i.zst.com.br/thumbs/12/3d/17/1273029423.jpg"
-        category={"Tênis"}
-        name="Nomad XYZ"
-        price={100}
-        priceDiscount={50}
-      />
-       <ProductCard
-        image="https://i.zst.com.br/thumbs/12/3d/17/1273029423.jpg"
-        category={"Tênis"}
-        name="Nomad XYZ"
-        price={100}
-        priceDiscount={50}
-      />
-       <ProductCard
-        image="https://i.zst.com.br/thumbs/12/3d/17/1273029423.jpg"
-        category={"Tênis"}
-        name="Nomad XYZ"
-        price={100}
-        priceDiscount={50}
-      />
+      <div className="flex flex-row flex-wrap justify-content-center">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            image={product.image}
+            category={product.category}
+            name={product.name}
+            price={product.price}
+            priceDiscount={product.priceDiscount}
+          />
+        ))}
+
       </div>
     </>
   );
