6769b828d6ae9a7e4fdb5a7ce6310c4d9fb56318
-- Anne Almeida -- <almeidaanne2025@gmail.com>
2025-06-18 21:19:48 -0300

    incluído logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src="./src/components/logo-header.svg">Drip Store</a>
+        </div>
+    );
+};
+
+export default Logo;
